c1d53b88939aa9940f5f4a2b4482b85101bf619b
nghib20 <nghib20@freeuni.edu.ge>
2024-05-14 12:02:49 +0400

    Added model classes and column mapping

---
+++ WeatherCRUD/Model/Temperature.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WeatherCRUD.Model
+{
+    public class Temperature
+    {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("city_id")]
+        public long CityId { get; set; }
+
+        [Column("temperature")]
+        public float TemperatureValue { get; set; }
+        
+        [Column("time")]
+        public DateTime Time { get; set; }
+    }
+}
--- WeatherCRUD/Program.cs
+++ WeatherCRUD/Program.cs
@@ -1,5 +1,51 @@
+using Dapper;
+using MySqlConnector;
+using System.ComponentModel.DataAnnotations.Schema;
+using WeatherCRUD.Model;
+
+SqlMapper.SetTypeMap(
+    typeof(City),
+    new CustomPropertyTypeMap(
+        typeof(City),
+        (type, columnName) =>
+            type.GetProperties().FirstOrDefault(prop =>
+                prop.GetCustomAttributes(false)
+                    .OfType<ColumnAttribute>()
+                    .Any(attr => attr.Name == columnName))));
+
+SqlMapper.SetTypeMap(
+    typeof(Temperature),
+    new CustomPropertyTypeMap(
+        typeof(Temperature),
+        (type, columnName) =>
+            type.GetProperties().FirstOrDefault(prop =>
+                prop.GetCustomAttributes(false)
+                    .OfType<ColumnAttribute>()
+                    .Any(attr => attr.Name == columnName))));
+//Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+using var connection = new MySqlConnection("Server=localhost;Port=3306;User ID=root;Password=rootroot;Database=weather_db");
+connection.Open();
+
+var sql = "SELECT * FROM city";
+var cities = connection.Query<City>(sql);
+
+foreach (var city in cities)
+{
+    Console.WriteLine($"{city.Id} {city.CityName}");
+}
+
+sql = "SELECT * FROM temperature";
+var temps = connection.Query<Temperature>(sql);
+
+foreach (var temp in temps)
+{
+    Console.WriteLine($"{temp.Id} {temp.CityId} {temp.TemperatureValue} {temp.Time}");
+}
 
 app.MapGet("/", () => "Hello World!");
 
